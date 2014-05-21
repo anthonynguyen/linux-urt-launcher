d7e4798c55593aacf9e14fe041eb4911e1627223
Anthony Nguyen <anknguyen@gmail.com>
2014-05-20 20:42:00 -0400

    Added support for URL parameters

--- urt-handler.py
+++ urt-handler.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import subprocess
 import sys
+import urlparse
 
 
 # Enter the path to your UrT executable
 URT_EXECUTABLE = "/home/clearskies/UrbanTerror42/Quake3-UrT.i386"
 
 
-subprocess.call([URT_EXECUTABLE, "+connect", sys.argv[1][6:]])
+url = urlparse.urlparse(sys.argv[1])
+arg = [URT_EXECUTABLE, "+connect", url.netloc]
+
+qs = urlparse.parse_qsl(url.query)
+for kv in qs:
+	arg.append("+" + kv[0])
+	arg.append(kv[1])
+
+subprocess.call(arg)
